a5f17800183181297a0e44a3691c20412f7f9cf1
Hasret özdemir <hasretozdemir288@gmail.com>
2025-12-27 21:17:55 +0300

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import '../models/filter_models.dart'; // PriceRange modelini tanuması için
+import '../models/filter_models.dart'; 
 
 class AppConstants {
   static const Color primaryBackgroundColor = Color.fromARGB(255, 4, 23, 36);
   static const Color accentColor = Color.fromARGB(255, 13, 75, 79);
   static const Color priceColor = Color.fromARGB(255, 4, 2, 28);
 
-  // --- DUMMY_DATA'DAN TAŞINAN VERİLER ---
 
-  // ODA KAPASİTESİ FİLTRESİ
+
+
   static const List<int> roomSizes = [1, 2, 3, 4];
 
-  // FİYAT ARALIĞI FİLTRESİ
+
   static final List<PriceRange> priceRanges = [
     PriceRange(0, 5000),
     PriceRange(5000, 10000),
